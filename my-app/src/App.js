1f5fea260446b41d1cfbd48f6514b235d55f30d0
KPbICA-CTATUCT <z.artemznahar@mail.ru>
2020-06-14 23:43:54 +0300

    v0.0.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,10 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://github.com/KPbICA-CTATUCT"
-          target="_blank"
-          rel="noopener noreferrer"
+          
         >
           Learn React
         </a>
