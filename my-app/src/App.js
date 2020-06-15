32cf743d31c9ed6fecd4d57b5245b8cdeb825f14
KPbICA-CTATUCT <z.artemznahar@mail.ru>
2020-06-15 20:09:05 +0300

    v 0.0.5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import TodoList from './Todo/TodoList';
+
 
 
 function App() {
-  return (
-    <div />
-  );
+  const todos = [
+    {id: 1, completed: false, title: "buy"},
+    {id: 2, completed: false, title: "sell"},
+    {id: 3, completed: false, title: "resell"}
+  ]
+
+
+  return <div className='wrapper'>
+    <h1>Todo List</h1>
+    <TodoList todos = {
+      todos
+    }>
+
+    </TodoList>
+  </div>;
 }
 
 export default App;
