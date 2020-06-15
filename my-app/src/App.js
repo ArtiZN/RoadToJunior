22afcc2b9371d77aad7ac3f143f96a0dfce8c505
KPbICA-CTATUCT <z.artemznahar@mail.ru>
2020-06-15 16:56:49 +0300

    The project is clean now

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        
-          <iframe src="https://www.google.com/"></iframe>
-        
-
-        <p>
-          <iframe src="https://www.247checkers.com/">
-            Говнокод на месте?
-          </iframe>
-        </p>
-        
-        
-      </header>
-    </div>
+    <div />
   );
 }
 
